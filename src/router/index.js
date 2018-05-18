7988b4772f41b321855be2c4dfb5483bea609355
zhangguangpeng <zhangguangpemng503@126.com>
2018-05-18 14:38:31 +0800

    1、优化域名访问 2、优化异步加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
 
 Vue.use(Router)
 
@@ -10,12 +8,12 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home')
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     }
   ]
 })
